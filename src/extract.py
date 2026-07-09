f5724f1de99512e26afc7e1a33e0712036b15260
vphngt <phuongvytran2109@gmail.com>
2026-07-09 07:29:07 -0400

    Update data cleaning notebook

--- src/extract.py
+++ src/extract.py
@@ -1,35 +1,92 @@
 import requests
+import pandas as pd
+from pathlib import Path
 
-base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
- 
-def get_package_resources(package_name):
-    url = f"{base_url}/api/3/action/package_show"
-    params = { "id": package_name }
-    package = requests.get(url, params=params)
-    package.raise_for_status() # safety net
-    return package.json()['result']['resources']
- 
-# To get resource data:
-for idx, resource in enumerate(package["result"]["resources"]):
- 
-       # for datastore_active resources:
-       if resource["datastore_active"]:
- 
-           # To get all records in CSV format:
-           url = f"{base_url}/datastore/dump/{resource['id']}"
-           resource_dump_data = requests.get(url).text
-           print(resource_dump_data)
- 
-           # To selectively pull records and attribute-level metadata:
-           url = f"{base_url}/api/3/action/datastore_search"
-           p = { "id": resource["id"] }
-           resource_search_data = requests.get(url, params = p).json()["result"]
-           print(resource_search_data)
-           # This API call has many parameters. They're documented here:
-           # https://docs.ckan.org/en/latest/maintaining/datastore.html
- 
-       # To get metadata for non datastore_active resources:
-       if not resource["datastore_active"]:
-           url = f"{base_url}/api/3/action/resource_show?id={resource['id']}"           
-           resource_metadata = requests.get(url).json()
-           print(resource_metadata)
+# ----- Configuration -----
+BASE_URL = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+
+DATASETS = {
+    "bus": "ttc-bus-delay-data",
+    "streetcar": "ttc-streetcar-delay-data",
+    "subway": "ttc-subway-delay-data"
+}
+
+RAW_DIR = Path("../data/raw")
+RAW_DIR.mkdir(parents=True, exist_ok=True)
+
+
+# ----- Functions -----
+def get_package(package_id):
+    url = BASE_URL + "/api/3/action/package_show"
+    response = requests.get(url, params={"id": package_id})
+    response.raise_for_status()
+
+    return response.json()["result"]
+
+
+def is_delay_dataset(resource):
+    return (
+        "2025" in resource["name"].lower()
+        and resource["format"].lower() == "csv"
+    )
+
+
+def is_lookup_dataset(resource):
+    return (
+        "code" in resource["name"].lower()
+        and resource["format"].lower() == "csv"
+    )
+
+def download_dataset(mode, package_id):
+    print(f"\n========== {mode.upper()} ==========")
+    package = get_package(package_id)
+
+    for resource in package["resources"]:
+        # only download necessary csv
+        if not is_delay_dataset(resource) and not is_lookup_dataset(resource):
+            continue
+
+        print(f"Resource: {resource['name']}")
+        print(f"Format: {resource['format']}")
+        print(f"Datastore Active: {resource['datastore_active']}")
+
+        extension = resource["format"].lower()
+
+        if is_lookup_dataset(resource):
+            file_name = f"{package_id}-lookup.{extension}"
+        else:
+            file_name = f"{package_id}-2025.{extension}"
+
+        file_path = RAW_DIR / file_name
+
+        # if they stores it in the datastore
+        if resource["datastore_active"]:
+            url = BASE_URL + "/datastore/dump/" + resource["id"]
+            print("Downloading CSV...")
+
+            csv_data = requests.get(url)
+            csv_data.raise_for_status()
+            with open(file_path, "wb") as f:
+                f.write(csv_data.content)
+
+            print(f"Saved to {file_path}")
+
+        # otherwise download directly from file URL
+        else:
+            file_url = resource["url"]
+            print("Downloading file...")
+
+            file_data = requests.get(file_url)
+            file_data.raise_for_status()
+            with open(file_path, "wb") as f:
+                f.write(file_data.content)
+
+            print(f"Saved to {file_path}")
+
+
+# ----- Main -----
+if __name__ == "__main__":
+    for mode, package_id in DATASETS.items():
+        download_dataset(mode, package_id)
+
+    print("\nAll datasets downloaded successfully!")
